70da73067fea098a0fd839bffab8cb3f7e4e430a
Tommy Huang <htommyf12@gmail.com>
2021-12-14 01:14:18 +0800

    added basic window on linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int main(void) {
+  GLFWwindow *window;
+
+  if (!glfwInit())
+    return -1;
+
+  window = glfwCreateWindow(640, 480, "Hello world", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }
+
